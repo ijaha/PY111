2e630e89c7feb0e0dc7b7ca5140d9847ea63867c
ijaha <Tiara1960>
2021-07-01 21:21:08 +0300

    merge done

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,33 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+
+    def merge(left_part, right_part):
+
+
+        result = []
+        while left_part and right_part:
+            if left_part[0] > right_part[0]:
+                result.append(right_part.pop(0))
+            else:
+                result.append(left_part.pop(0))
+
+        if left_part:
+            result.extend(left_part)
+        elif right_part:
+            result.extend(right_part)
+
+        return result
+
+    if len(container) == 1 or not container:
+        return container
+
+
+    left = container[:len(container) // 2]
+    right = container[len(container) // 2:]
+
+    left = sort(left)
+    right = sort(right)
+
+
+    return merge(left, right)
