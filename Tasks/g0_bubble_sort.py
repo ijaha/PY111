a7c7aaa488e1e08eec47628f433293454442d274
ijaha <Tiara1960>
2021-07-01 20:27:13 +0300

    sorted done

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,16 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    def swap(left_index: int, right_index: int):
+        left_value = container[left_index]
+        right_value = container[right_index]
+
+        if left_value > right_value:
+            container[right_index] = left_value
+            container[left_index] = right_value
+
+    for j in range(len(container)):
+        for i in range(len(container) - 1 - j):
+            swap(i, i+1)
+
     return container
